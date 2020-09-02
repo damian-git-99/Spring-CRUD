91b261e4219bf2ac758b74a62b20224e8aa70615
Damian-Escritorio <damiangalvan66@gmail.com>
2020-09-02 18:42:22 -0500

    Estilos mejorados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,9 @@
 INSERT INTO clientes (id,nombre,apellido,email,create_at) values (1,'Damian','Galvan','damian@gmil.com','2020-08-28')
 INSERT INTO clientes (id,nombre,apellido,email,create_at) values (2,'Jose','Torrez','jose@gmil.com','2020-08-28')
+INSERT INTO clientes (id,nombre,apellido,email,create_at) values (3,'irving','Galvan','damian@gmil.com','2020-08-28')
+INSERT INTO clientes (id,nombre,apellido,email,create_at) values (4,'Carlos','Torrez','jose@gmil.com','2020-08-28')
+INSERT INTO clientes (id,nombre,apellido,email,create_at) values (5,'Jazmin','Galvan','damian@gmil.com','2020-08-28')
+INSERT INTO clientes (id,nombre,apellido,email,create_at) values (6,'Robin','Torrez','jose@gmil.com','2020-08-28')
+INSERT INTO clientes (id,nombre,apellido,email,create_at) values (7,'Mateo','Galvan','damian@gmil.com','2020-08-28')
+INSERT INTO clientes (id,nombre,apellido,email,create_at) values (8,'Talina','Torrez','jose@gmil.com','2020-08-28')
 
-INSERT INTO empleados (id,nombre,apellido,email) values (56,'Carlos','Gimenez','Carlos@gmil.com')
-INSERT INTO empleados (id,nombre,apellido,email) values (58,'Jair','lopez','Jair@gmil.com')
