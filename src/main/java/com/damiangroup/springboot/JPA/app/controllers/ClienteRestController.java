8b0ac9f4b3003c353a5832db3b959dc56f7795a9
damian <damiangalvan66@gmail.com>
2020-12-22 18:31:12 -0600

    Ejemplo de restController

---
+++ src/main/java/com/damiangroup/springboot/JPA/app/controllers/ClienteRestController.java
@@ -0,0 +1,25 @@
+package com.damiangroup.springboot.JPA.app.controllers;
+
+import com.damiangroup.springboot.JPA.app.models.entity.Cliente;
+import com.damiangroup.springboot.JPA.app.models.service.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/clientes")
+public class ClienteRestController {
+
+    @Autowired
+    private IClienteService clienteService;
+
+    @GetMapping("/listar")
+    public List<Cliente> listar(){;
+        return clienteService.findAll();
+    }
+
+}
