a3206e2d0e5c5eb93b7428d6de7cde65dc1aa072
damian <damiangalvan66@gmail.com>
2023-03-18 16:43:36 -0600

    remove unnecessary code

--- src/main/java/com/damiangroup/springboot/JPA/app/SpringBootJpaApplication.java
+++ src/main/java/com/damiangroup/springboot/JPA/app/SpringBootJpaApplication.java
@@ -1,33 +1,13 @@
 package com.damiangroup.springboot.JPA.app;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @SpringBootApplication
-public class SpringBootJpaApplication implements CommandLineRunner {
-
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJpaApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		String password = "1234";
-		
-		for (int i = 0; i < 2; i++) {
-			String bcryptaPassword = passwordEncoder.encode(password);
-			System.out.println(bcryptaPassword);
-		}
-		
-	}
-
-	
-
+public class SpringBootJpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaApplication.class, args);
+    }
 }
