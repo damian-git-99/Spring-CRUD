1309bb09c61843872be806d8c4ab6980dfd4deef
damian <damiangalvan66@gmail.com>
2023-04-20 10:07:30 -0600

    style(CustomerDao.java): remove trailing whitespaces feat(CustomerDaoCrudRepository.java): add new file for CustomerDaoCrudRepository interface style(ICrudDao.java): add blank lines between methods and align method signatures

--- src/main/java/com/damiangroup/springboot/JPA/app/models/dao/CustomerDaoCrudRepository.java
+++ src/main/java/com/damiangroup/springboot/JPA/app/models/dao/CustomerDaoCrudRepository.java
@@ -3,12 +3,8 @@ package com.damiangroup.springboot.JPA.app.models.dao;
 import com.damiangroup.springboot.JPA.app.models.entity.Cliente;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface IClienteDaoCrudRepository extends PagingAndSortingRepository<Cliente, Long>{
-
-	//@Query("Select * from Cliente c where c.email = ?1 ")
-	//Cliente findByEmail(String email);
-	
-	
-	
+public interface CustomerDaoCrudRepository extends PagingAndSortingRepository<Cliente, Long> {
+    //@Query("Select * from Cliente c where c.email = ?1 ")
+    //Cliente findByEmail(String email);
 }
 
