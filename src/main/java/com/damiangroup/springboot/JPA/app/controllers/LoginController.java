846b2e00b9055d7a604e036ec470d34ded77a28d
damian <damiangalvan66@gmail.com>
2023-04-20 12:07:09 -0600

    style(LoginController.java): reformat code for better readability feat(login.html): add language attribute to html tag and change login form layout

--- src/main/java/com/damiangroup/springboot/JPA/app/controllers/LoginController.java
+++ src/main/java/com/damiangroup/springboot/JPA/app/controllers/LoginController.java
@@ -10,28 +10,27 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
-	
-	@GetMapping("/login")
-	public String login(@RequestParam(value = "error",required = false) String error,
-			@RequestParam(value = "logout",required = false) String logout
-			,Model model, Principal principal, RedirectAttributes flash) { // principal es el user
-		
-		if (principal!=null) {
-			flash.addFlashAttribute("info","Ya ha iniciado sesión");
-			return "redirect:/";
-		}
-		
-		if (error!=null) {
-			model.addAttribute("error","Error en el login: Nombre de usuario o contraseña incorrecto");
-		}
-		
-		if (logout!=null) {
-			model.addAttribute("success","Sesión cerrada con exito");
-		}
-		
-		return "login";
-	}
-	
-	
+
+    @GetMapping("/login")
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        @RequestParam(value = "logout", required = false) String logout
+            , Model model, Principal principal, RedirectAttributes flash) {
+
+        if (principal != null) {
+            flash.addFlashAttribute("info", "You have already logged in");
+            return "redirect:/";
+        }
+
+        if (error != null) {
+            model.addAttribute("error", "Error: Invalid username or password");
+        }
+
+        if (logout != null) {
+            model.addAttribute("success", "Session closed successfully");
+        }
+
+        return "login";
+    }
+
 
 }
