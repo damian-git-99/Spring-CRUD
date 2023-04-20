04b45fba669b16f84fa455f7ea9452de70acc909
damian <damiangalvan66@gmail.com>
2023-04-20 11:11:11 -0600

    refactor(JpaUserDetailsService.java): change UserDao field to final and inject it through constructor feat(JpaUserDetailsService.java): remove unnecessary arguments from UserDetails constructor fix(JpaUserDetailsService.java): change exception messages to english

--- src/main/java/com/damiangroup/springboot/JPA/app/models/service/JpaUserDetailsService.java
+++ src/main/java/com/damiangroup/springboot/JPA/app/models/service/JpaUserDetailsService.java
@@ -3,6 +3,7 @@ package com.damiangroup.springboot.JPA.app.models.service;
 import java.util.ArrayList;
 import java.util.List;
 import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,30 +17,33 @@ import com.damiangroup.springboot.JPA.app.models.entity.User;
 
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
+    
+    private final UserDao userDao;
 
-	@Autowired
-	private UserDao usuarioDao;
+    @Autowired
+    public JpaUserDetailsService(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
-	@Override
-	@Transactional
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = usuarioDao.fetchByUsername(username);
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.fetchByUsername(username);
 
-		if (user == null) {
-			throw new UsernameNotFoundException("El user no existe");
-		}
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
 
-		List<GrantedAuthority> authorities = new ArrayList<>();
-		for (Role role : user.getRoles()) {
-			authorities.add(new SimpleGrantedAuthority(role.getAuthority()));
-		}
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : user.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority(role.getAuthority()));
+        }
 
-		if (authorities.isEmpty()) {
-			throw new UsernameNotFoundException("El user no tiene roles asignados");
-		}
+        if (authorities.isEmpty()) {
+            throw new UsernameNotFoundException("User has no authorities");
+        }
 
-		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.isEnabled(), true, true, true,
-				authorities);
-	}
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
 
 }
